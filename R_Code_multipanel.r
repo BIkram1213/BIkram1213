86a12323e491c92756d894974ec76f17618904c1
BIkram1213 <62661788+BIkram1213@users.noreply.github.com>
2020-03-27 18:09:10 +0100

    Create R_Code_multipanel.r

---
+++ R_Code_multipanel.r
@@ -0,0 +1,15 @@
+### Multipanel in R
+install.packages("GGally") # used for gg pairs()
+library("sp") #require(sp) same
+library(GGally)
+
+data(meuse)
+attach(meuse
+#make all the possible pairwise plot of the dataset
+pairs(meuse)
+pairs(~cadmium+copper+lead+zinc,data=meuse)
+pairs(meuse[,3:6])
+pairs(meuse[,3:6], col="red",pch=21,cex=2)
+GGally for beautification
+ggpairs(meuse[,3:6])
+ggpairs(meuse[,3:6], col="red")
