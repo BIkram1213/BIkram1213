d10e60fed0ea15b85820f6d917055a09a1160cf1
BIkram1213 <62661788+BIkram1213@users.noreply.github.com>
2020-04-24 17:18:03 +0200

    Update R_Code_multipanel.r

--- R_Code_multipanel.r
+++ R_Code_multipanel.r
@@ -10,6 +10,6 @@ pairs(meuse)
 pairs(~cadmium+copper+lead+zinc,data=meuse)
 pairs(meuse[,3:6])
 pairs(meuse[,3:6], col="red",pch=21,cex=2)
-GGally for beautification
+#GGally for beautification
 ggpairs(meuse[,3:6])
 ggpairs(meuse[,3:6], col="red")
