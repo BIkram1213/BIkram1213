9b5ed2c25fa7af808d9d844c1b1d47138f5a019f
BIkram1213 <62661788+BIkram1213@users.noreply.github.com>
2020-07-15 21:25:22 +0200

    Update R_ME_first.r

--- R_ME_first.r
+++ R_ME_first.r
@@ -4,6 +4,7 @@ data(meuse)
 # Look at the meuse dataset
 meuse
 # Look at the first row of the dataset
+# # used for explaination, # does not read the data
 head(meuse)
 # lets plot two variables
 attach(meuse)
@@ -11,3 +12,4 @@ plot(zinc,copper)
 plot(zinc,copper, col="blue")
 plot(zinc,copper, col="blue",pch=22)
 plot(zinc,copper, col="blue",pch=22,cex=3)
+# col is used to change the color, pch is is to chnage the shapes and cex is the scale of text and symbols
