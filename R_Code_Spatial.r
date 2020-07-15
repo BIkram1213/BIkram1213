d7d1f9c1b93fabcb011600d74306a7629a62ba17
BIkram1213 <62661788+BIkram1213@users.noreply.github.com>
2020-07-15 23:17:41 +0200

    Update R_Code_Spatial.r

--- R_Code_Spatial.r
+++ R_Code_Spatial.r
@@ -1,38 +1,31 @@
 install.packages("sp")
 library(sp)
-
 data(meuse)
-
 head(meuse)
 
 # coordinates
-coordinates(meuse) = ~x+y      # alt+126
+coordinates(meuse) = ~x+y      
 
 plot(meuse)
-
 spplot(meuse, "zinc")
 
-# Exercise: plot the spatial amount of copper
+# Plot the spatial amount of copper
 spplot(meuse, "copper", main="Copper concentration")
-
+# main is used to provide headings to the graph/table
 bubble(meuse, "zinc")
 bubble(meuse, "zinc", main="Zinc concentration")
-
+# Bubble used to make the points bigger and more readable
 # Exercise: bubble copper in red
 bubble(meuse, "copper", main="Cppoer concentration", col="red")
 
-#### Importing new data
+# Importing new data
 
-# download covid_agg.csv from our teaching site and build a folder called lab into C: 
+# download covid_agg.csv file and build a folder called lab into C: 
 # put the covid_agg.csv file into the folder lab
 
 # setting the working directory: lab
-# Windows
-# setwd("C:/lab/")
-# Mac users
-# setwd("/Users/yourname/lab/")
-# Linux users
-setwd("~/lab")
+# setwd("C:/lab/") for windows
+setwd("C:/lab")
 
 covid <- read.table("covid_agg.csv", head=T)
 head(covid)
@@ -41,12 +34,11 @@ attach(covid)
 plot(country,cases)
 
 # plot(covid$country,covid$cases)
-
 plot(country, cases, las=0) # parallel labels
 plot(country, cases, las=1) # horizontal labels
 plot(country, cases, las=2) # perpendicular labels
 plot(country, cases, las=3) # vertical labels
-
+# las changes the label directions in the axis
 plot(country, cases, las=3, cex.axis=0.5)
 plot(country, cases, las=3, cex.axis=0.7)
 
@@ -57,30 +49,19 @@ install.packages("ggplot2")
 
 # If ggplot2 cannot be found by the software: install the devtools package and then install 
 # install.packages("devtools")
-# devtools::install_github("tidyverse/ggplot2")
-
-# save the .RData under the menu File
-# for Windows users: save as "yourprefferredname.RData"
-
-
-# load the previously saved .RData
 
 # setting the working directory: lab
 # Windows
 # setwd("C:/lab/")
-# Mac users
-# setwd("/Users/yourname/lab/")
-# Linux users
-setwd("~/lab")
+setwd("C:/lab")
 
 load("spatial.RData")
-
 ls()
 # covid
 
 install.packages("ggplot2")
 library(ggplot2) #require(ggplot2)
-
+#ggplot is to beautify the graphs
 data(mpg)
 head(mpg)
 # key components: data, aes, geometry
@@ -90,8 +71,4 @@ ggplot(mpg, aes(x=displ,y=hwy)) + geom_polygon()
 
 head(covid)
 ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point()
-
-# Mac Users: in case you receive a graphical error, assign the ggplot to an object:
-# graph1 <- ggplot(covid, aes(x=lon, y=lat, size=cases)) + geom_point() e in seguito ho 
-# and then plot by:
-# plot(graphs_1) 
+ 
