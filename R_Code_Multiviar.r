58d547f3dcede3b3a0d093b01c1b88538cca9adc
BIkram1213 <62661788+BIkram1213@users.noreply.github.com>
2020-07-15 23:26:16 +0200

    Update R_Code_Multiviar.r

--- R_Code_Multiviar.r
+++ R_Code_Multiviar.r
@@ -2,3 +2,18 @@
 setwd("C:/lab/")
 biomes<-read.table("biomes.csv", header=T,sep=",")
 head(bomes) # view (biomes)
+# DEtrended CORrespondence ANAlysis
+multivar <- decorana(biomes)
+multivar
+
+plot(multivar)
+
+# biomes types
+biomes_types <- read.table("biomes_types.csv", header=T, sep=",")
+head(biomes_types)
+
+attach(biomes_types)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+# or col=1:4
+
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T) 
